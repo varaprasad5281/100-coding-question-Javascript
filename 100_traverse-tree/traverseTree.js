3a17ed355291b67acc4473b5100c6f557c601033
kanugula vara prasad <119512036+varaprasad5281@users.noreply.github.com>
2024-12-17 11:19:25 +0530

    Update traverseTree.js

--- 100_traverse-tree/traverseTree.js
+++ 100_traverse-tree/traverseTree.js
@@ -1,9 +1,10 @@
 export function traverseTree(node, callback) {
-  if (!node) return;
-
-  callback(node);
-
-  if (node.children && node.children.length > 0) {
-    node.children.forEach((child) => traverseTree(child, callback));
+    if (node === null) return; // Handle the case where the node is null
+  
+    callback(node); // Call the callback function for the current node
+  
+    // Recursively call traverseTree for all children of the current node
+    if (node.children && node.children.length > 0) {
+      node.children.forEach((child) => traverseTree(child, callback));
+    }
   }
-}
